21fd65f9ddc0ace32e1e3075e04b2901094a3266
suu3 <71166763+suu3@users.noreply.github.com>
2021-03-22 12:45:09 +0900

    #5.1

--- src/socketController.js
+++ src/socketController.js
@@ -1,10 +1,16 @@
 import events from "./events";
 
 const socketController = socket => {
+    const broadcast = (event, data) => socket.broadcast.emit(event, data);
+
     socket.on("setNickname", ({ nickname }) => {
-      console.log(nickname);
-      socket.nickname = nickname;
-      socket.broadcast.emit(events.newUser, { nickname });
+        console.log(nickname);
+        socket.nickname = nickname;
+        broadcast(events.newUser, { nickname });
+    });
+
+    socket.on(events.disconnect, () => {
+        broadcast(events.disconnected, { nickname: socket.nickname });
     });
 };
   
