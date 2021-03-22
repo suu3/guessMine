7159cb9261585ba21bcc1a9a01a6701c55b27597
suu3 <71166763+suu3@users.noreply.github.com>
2021-03-22 18:30:02 +0900

    #6.4

--- src/socketController.js
+++ src/socketController.js
@@ -17,21 +17,30 @@ const socketController = (socket, io) => {
             inProgress = true;
             const leader = chooseLeader();
             word = chooseWord();
+            io.to(leader.id).emit(events.leaderNotif, { word });
+            superBroadcast(events.gameStarted);
         }
     };
-
+    const endGame = () => {
+        inProgress = false;
+    };
 
     socket.on(events.setNickname, ({ nickname }) => {
         socket.nickname = nickname;
         sockets.push({ id: socket.id, points: 0, nickname: nickname });
         broadcast(events.newUser, { nickname });
         sendPlayerUpdate();
-        startGame();
+        if (sockets.length === 1) {
+            startGame();
+        }
     });
 
 
     socket.on(events.disconnect, () => {
         sockets = sockets.filter(aSocket => aSocket.id !== socket.id);
+        if (sockets.length === 1) {
+            endGame();
+        }
         broadcast(events.disconnected, { nickname: socket.nickname });
         sendPlayerUpdate();
     });
