5bc7f2b77bcaebc5c2e3d104cb325428938f51fe
suu3 <71166763+suu3@users.noreply.github.com>
2021-03-22 13:37:06 +0900

    #5.3

--- assets/js/chat.js
+++ assets/js/chat.js
@@ -1,3 +1,5 @@
+import { getSocket } from "./sockets";
+
 const messages = document.getElementById("jsMessages");
 const sendMsg = document.getElementById("jsSendMsg");
 
@@ -15,10 +17,14 @@ const handleSendMsg = event => {
   event.preventDefault();
   const input = sendMsg.querySelector("input");
   const { value } = input;
+  getSocket().emit(window.events.sendMsg, { message: value });
   input.value = "";
   appendMsg(value);
 };
 
+export const handleNewMessage = ({ message, nickname }) =>
+  appendMsg(message, nickname);
+  
 if (sendMsg) {
   sendMsg.addEventListener("submit", handleSendMsg);
 }
